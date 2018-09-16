02a559b7c096257f4ccff4f053b0b90a8cbe89fc
Thomas Le moullec <thomas.le-moullec@epitech.eu>
2018-09-16 15:03:33 +0800

    blueprint

---
+++ blockchain.py
@@ -0,0 +1,19 @@
+class Blockchain(object):
+
+    def __init__(self):
+        self.chain = []
+        self.payload = []
+
+    def new_block(self):
+        pass
+
+    def new_payload(self):
+        pass
+
+    @staticmethod
+    def hash(block):
+        pass
+
+    @property
+    def get_last_block(self):
+        pass
