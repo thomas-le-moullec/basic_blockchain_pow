e71b090745c7c84d7a1b449b8e4ba0fc54c9b174
Thomas Le moullec <thomas.le-moullec@epitech.eu>
2018-09-16 21:22:08 +0800

    PoW with sha512

--- blockchain.py
+++ blockchain.py
@@ -45,3 +45,16 @@ class Blockchain(object):
     @property
     def get_last_block(self):
         return self.chain[-1]
+
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha512(guess).hexdigest()
+        return guess_hash[:4] == "0000"
