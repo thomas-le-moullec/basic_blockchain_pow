223cb0bf58d6ff187f5ca8f89275352286f23c5f
Thomas Le moullec <thomas.le-moullec@epitech.eu>
2018-09-17 10:49:41 +0800

    Refactoring : Split Blockchain and API

--- blockchain.py
+++ blockchain.py
@@ -1,10 +1,7 @@
 import hashlib
 import json
 
-from textwrap import  dedent
 from time import time
-from uuid import uuid4
-from flask import Flask, jsonify, request
 
 class Blockchain(object):
 
@@ -27,11 +24,14 @@ class Blockchain(object):
         # Reset list of transactions
         self.current_tx = []
 
+        # Add the new block to the blockchain
         self.chain.append(newBlock)
         return newBlock
 
     def new_payload(self, sender, recipient, message):
 
+        # New message posted. Currently only a message from an address to another one.
+        # For a forum we need to add the name of the channel
         self.current_tx.append({
             'sender' : sender,
             'recipient' : recipient,
@@ -42,14 +42,18 @@ class Blockchain(object):
 
     @staticmethod
     def hash(block):
+        # Hash with SHA512
         block_json = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha512(block_json).hexdigest()
 
     @property
     def get_last_block(self):
+        # Return the last block of the blockchain
         return self.chain[-1]
 
     def proof_of_work(self, last_proof):
+        # Proof of work.
+        # For forum without any remuneration on mining proof of work(PoW) can be replace by another verification method
         proof = 0
         while self.valid_proof(last_proof, proof) is False:
             proof += 1
@@ -58,61 +62,8 @@ class Blockchain(object):
 
     @staticmethod
     def valid_proof(last_proof, proof):
+        # To validate the proof of work:
+        # We check if the hash (SHA512) of last_proof and current one contains 5 leading zeroes.
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha512(guess).hexdigest()
         return guess_hash[:4] == "0000"
-
-app = Flask(__name__)
-
-node_identifier = str(uuid4()).replace('-', '')
-
-blockchain = Blockchain()
-
-@app.route('/mine', methods=['GET'])
-def mine():
-    last_block = blockchain.get_last_block
-    last_proof = last_block['proof']
-    proof = blockchain.proof_of_work(last_proof)
-
-    # New payload to normally pay the miner
-    blockchain.new_payload(
-        sender="0",
-        recipient=node_identifier,
-        message="Thanks for mining"
-    )
-
-    previous_hash = blockchain.hash(last_block)
-    block = blockchain.new_block(proof, previous_hash)
-
-    response = {
-        'message': "New Block Forged",
-        'index': block['index'],
-        'messages': block['payloads'],
-        'proof': block['proof'],
-        'previous_hash': block['previous_hash'],
-    }
-    return jsonify(response), 200
-
-@app.route('/messages/new', methods=['POST'])
-def new_message():
-    data = request.get_json()
-    required = ['sender', 'recipient', 'message']
-    if not all(k in data for k in required):
-        return 'Missing Values', 400
-
-    # Create a new message
-    index = blockchain.new_payload(data['sender'], data['recipient'], data['message'])
-
-    response = {'response' : f'Tx will be added to Block {index}'}
-    return jsonify(response), 201
-
-@app.route('/chain', methods=['GET'])
-def full_chain():
-    response = {
-        'chain' : blockchain.chain,
-        'size' : len(blockchain.chain),
-    }
-    return jsonify(response), 200
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
